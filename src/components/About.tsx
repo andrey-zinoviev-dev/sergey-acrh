248a531b81ebf9887bc712d4fa188ef6630acfac
Andrey <55049581+andrey-zinoviev-dev@users.noreply.github.com>
2026-02-17 01:31:58 +0300

    updating projects

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -36,51 +36,7 @@ export default function About() {
                             steps={aboutSteps}
                         />
                     </div>
-                    {/* <p>Архитектура для меня - это живой процесс, а каждое из этих напрвлений - это новый сценарий жизни.</p> */}
-                    {/* <Headline className={styles.aboutHeading}>&quot;АРХИТЕКТУРА – <br />
-                        ЭТО ЖИВОЙ ПРОЦЕСС.&quot;
-                    </Headline> */}
-                    {/* <div className={styles.aboutDescriptionContent}>
-                        <p className={styles.aboutParagraph}>
-                            Я — частный архитектор.
-                        </p>
-                        <p className={styles.aboutParagraph}>
-                            Специализируюсь на создании объектов с высокой художественной ценностью: от реставрации наследия до новых зданий с уникальным характером.
-                        </p>
-                        <p className={styles.aboutParagraph}>
-                            Мной движет стремление сохранить чистоту идеи на всех этапах и создать архитектуру, которая имеет значение.
-                        </p>
-                    </div>
-                    <div className={styles.imageWrapper}>
-                        <Image
-                            src="/about-rect.png"
-                            alt="О нас"
-                            width={600}
-                            height={600}
-                            className={styles.image}
-                        />
-                        <div className={styles.authorContent}>
-                            <Headline className={styles.authorHeading}>Сергей Ильич Пономаренко</Headline>
-                            <p className={styles.authorParagraph}>Магистр МАРХИ, член Союза Архитекторов.</p>
-                        </div>
-                    </div> */}
-
                 </div>
-                <div className={styles.content}>
-                    {/* <Headline className={styles.aboutHeading}>&quot;АРХИТЕКТУРА – ЭТО ЖИВОЙ ПРОЦЕСС.&quot;</Headline>
-                    <div className={styles.textBlock}>
-                        <p className={[styles.paragraph, styles.author].join(' ')}><span>Сергей Ильич Пономаренко</span> <span>Магистр МАРХИ, член Союза Архитекторов.</span></p>
-                        <p className={styles.paragraph}>Я — частный архитектор. Специализируюсь на создании объектов с высокой художественной ценностью: от реставрации наследия до новых зданий с уникальным характером.</p>
-                        <p className={styles.paragraph}>Мной движет стремление сохранить чистоту идеи на всех этапах и создать архитектуру, которая имеет значение.</p>
-                    </div> */}
-                </div>
-                {/* <Image
-                    src="/about-rect.png"
-                    alt="О нас"
-                    width={600}
-                    height={600}
-                    className={styles.image}
-                /> */}
             </Container>
         </section>
     );
